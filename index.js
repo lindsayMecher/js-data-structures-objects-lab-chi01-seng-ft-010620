572199b14192f3955fb1fec441457fa2908ab7c2
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-19 23:33:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("dom loaded")
+  // const driver = {};
+  // updateDriverWithKeyAndValue()
+  // destructivelyUpdateDriverWithKeyAndValue()
+  // deleteFromDriverByKey()
+  // destructivelyDeleteFromDriverByKey()
+})
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const copyOfDriver = { ...driver };
+  copyOfDriver[key] = value;
+  return copyOfDriver;
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+};
+
+function deleteFromDriverByKey(driver, key) {
+  const copyOfDriver = { ...driver };
+  delete copyOfDriver[key];
+  return copyOfDriver;
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+};
