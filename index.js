fd1ed40f12a6fd0eda16205b293368950427a1e9
Lindsay Mecher <lindsaymecher@gmail.com>
2020-10-27 22:17:35 -0400

    finished deliverables with arrow functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+
+let driver = {};
+
+const updateDriverWithKeyAndValue = (driver, key, value) => {
+    const newObj = Object.assign({}, driver);
+    newObj[key] = value;
+    return newObj;
+}
+
+const destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) => {
+    driver[key] = value;
+    return driver;
+}
+
+const deleteFromDriverByKey = (driver, key) => {
+    const newObj = Object.assign({}, driver);
+    delete newObj[key];
+    return newObj;
+}
+
+const destructivelyDeleteFromDriverByKey = (driver, key) => {
+    delete driver[key];
+    return driver;
+}
+
